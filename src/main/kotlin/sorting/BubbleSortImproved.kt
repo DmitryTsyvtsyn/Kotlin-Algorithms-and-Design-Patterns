5eb4810a7343dd672f063f9e5e6b704dbf7f27d4
DmitryTsyvtsyn <dmitry.kind.2@gmail.com>
2021-10-18 12:28:36 +0600

    добавление строки документации к bubble sort и реализация BubbleSortImproved

--- src/main/kotlin/sorting/BubbleSortImproved.kt
+++ src/main/kotlin/sorting/BubbleSortImproved.kt
@@ -1,23 +1,26 @@
 package sorting
 
 /**
- * сортировка пузырьком является одной из самых простых и интуитивно понятных
- * алгоритмов сортировки.
+ * улучшенный вариант пузырьковой сортировки: если массив уже отсортирован, то после
+ * первого прохода по массиву, мы завершаем выполнение алгоритма за время n
  * худший случай: n²
  * лучший случай: n
  * средний случай: n²
  */
-class BubbleSort<T : Comparable<T>> : SortAlgo<T>() {
+class BubbleSortImproved<T : Comparable<T>> : SortAlgo<T>() {
 
     override fun sortAlgo(array: Array<T>) {
+        var isSorted = true
         for (i in 0 until array.size - 1) {
             for (j in 0 until array.size - 1 - i) {
                 if (array[j] > array[j + 1]) {
+                    isSorted = false
                     array[j] = array[j + 1].apply {
                         array[j + 1] = array[j]
                     }
                 }
             }
+            if (isSorted) break
         }
     }
 
--- src/main/kotlin/sorting/main.kt
+++
@@ -1,5 +0,0 @@
-package sorting
-
-fun main() {
-    print(BubbleSort<Int>().sort(arrayOf(3, 6, 10, 20, 1000)).toList())
-}
