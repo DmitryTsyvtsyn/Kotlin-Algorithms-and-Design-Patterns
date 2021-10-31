24f97b18356b48e75d91b0893078e95029d4a30e
DmitryTsyvtsyn <dmitry.kind.2@gmail.com>
2021-10-31 18:39:21 +0600

    добавление паттерна Strategy

---
+++ src/test/kotlin/design_patterns/RubleExchangeRateTest.kt
@@ -0,0 +1,19 @@
+package design_patterns
+
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+
+internal class RubleExchangeRateTest {
+
+    @Test
+    fun test() {
+
+        val exchange = RubleExchangeRate()
+        assertEquals(1.0, exchange.exchange(70.0))
+
+        exchange.changeStrategy(ExchangeStrategy.Tenge())
+        assertEquals(60.0, exchange.exchange(10.0))
+
+    }
+
+}
