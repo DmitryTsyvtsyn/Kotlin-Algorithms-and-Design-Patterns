3430eb20d1bbefe018207317f3e8f93ca00d8835
DmitryTsyvtsyn <dmitry.kind.2@gmail.com>
2021-10-31 09:42:39 +0600

    добавление паттерна Fluent Interface

---
+++ src/main/kotlin/design_patterns/Fluent Interface Pattern.kt
@@ -0,0 +1,28 @@
+package design_patterns
+
+class ViewContext
+
+class View(ctx: ViewContext) {
+    private var isFocusable = false
+    private var isClickable = false
+    private var bgColor: Int = 0x444444
+
+    fun focusable() = isFocusable
+    fun clickable() = isClickable
+    fun bgColor() = bgColor
+
+    fun makeFocusable() = apply {
+        isFocusable = true
+    }
+
+    fun makeClickable() = apply {
+        isClickable = true
+    }
+
+    fun changeBackground(color: Int) = apply {
+        bgColor = color
+    }
+
+    fun draw() {}
+
+}
---
+++ src/test/kotlin/design_patterns/ViewTest.kt
@@ -0,0 +1,22 @@
+package design_patterns
+
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+
+internal class ViewTest {
+
+    @Test
+    fun test() {
+
+        val view = View(ViewContext()).changeBackground(0xffffff)
+            .makeFocusable()
+            .makeClickable()
+
+        view.draw()
+
+        assertEquals(0xffffff, view.bgColor())
+        assertEquals(true, view.clickable())
+        assertEquals(true, view.focusable())
+    }
+
+}
