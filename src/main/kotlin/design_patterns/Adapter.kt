9232f4114e077492d428262f5eda2f9be625b680
DmitryTsyvtsyn <dmitry.kind.2@gmail.com>
2021-11-03 09:18:37 +0600

    добавление паттерна Adapter

---
+++ src/main/kotlin/design_patterns/Adapter.kt
@@ -0,0 +1,49 @@
+package design_patterns
+
+/**
+ * интерфейс адаптера
+ */
+interface Adapter<T> {
+    fun getItem(position: Int) : T
+    fun getItemCount() : Int
+}
+
+/**
+ * паттерн Adapter
+ * здесь приведена упрощенная имитация RecyclerView компонента из Android
+ */
+class RecyclerView<T> {
+
+    /**
+     * хранит ссылку на адаптер
+     */
+    private var adapter: Adapter<T>? = null
+
+    /**
+     * изменяет текущий адаптер на новый
+     *
+     * @adapter - новый адаптер
+     */
+    fun changeAdapter(adapter: Adapter<T>) {
+        this.adapter = adapter
+    }
+
+    /**
+     * отрисовывает элементы из адаптера
+     *
+     * возвращает список элементов для тестирования
+     */
+    fun draw() : List<T> {
+        val items = mutableListOf<T>()
+        val myAdapter = adapter
+        if (myAdapter != null) {
+            val count = myAdapter.getItemCount()
+            for (i in 0 until count) {
+                items.add(myAdapter.getItem(i))
+                // draw item
+            }
+        }
+        return items
+    }
+
+}
