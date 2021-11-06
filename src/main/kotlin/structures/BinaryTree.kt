7f9efd617b810f1535ebdb6342bb377d1f57a96f
DmitryTsyvtsyn <dmitry.kind.2@gmail.com>
2021-11-06 09:31:28 +0600

    добавление строк документации для динамического массива и рефакторинг функций для бинарного дерева

--- src/main/kotlin/structures/BinaryTree.kt
+++ src/main/kotlin/structures/BinaryTree.kt
@@ -2,57 +2,11 @@ package structures
 
 import java.util.*
 
+
 /**
- * представляет узел дерева
+ * структура данных: бинарное дерево
  *
- * @value - значение узла
- * @left - левый дочерний элемент
- * @right - правый дочерний элемент
- */
-class Node(
-    private var value: Int,
-    private var left: Node? = null,
-    private var right: Node? = null
-) {
-    /**
-     * возвращает значение узла
-     */
-    fun value() = value
-
-    /**
-     * изменяет значение узла
-     */
-    fun changeValue(value: Int) {
-        this.value = value
-    }
-
-    /**
-     * изменяет левый дочерний узел
-     */
-    fun changeLeft(left: Node?) {
-        this.left = left
-    }
-
-    /**
-     * изменяет правый дочерний узел
-     */
-    fun changeRight(right: Node?) {
-        this.right = right
-    }
-
-    /**
-     * возвращает левый дочерний узел
-     */
-    fun leftNode() = left
-
-    /**
-     * возвращает правый дочерний узел
-     */
-    fun rightNode() = right
-}
-
-/**
- * бинарное дерево состоит из узлов, каждый из которых имеет максимум два дочерних элемента
+ * описание: состоит из узлов, каждый из которых имеет максимум два дочерних элемента,
  * дочерние узлы удовлетворяют следующим требованиям:
  *  - левый дочерний элемент меньше родителя;
  *  - правый дочерний элемент больше родителя;
@@ -66,16 +20,18 @@ class BinaryTree {
     private var root: Node? = null
 
     /**
-     * добавление в дерево нового значения
+     * добавление в дерево нового элемента
      *
-     * @value - значение
+     * @value - значение элемента
      */
     fun add(value: Int) {
         root = addRec(root, value)
     }
 
     /**
-     * возвращает true, если дерево не содержит ни одного узла
+     * проверяет дерево на пустоту
+     *
+     * @return возвращает true, если дерево не содержит ни одного узла
      */
     fun isEmpty() = root == null
 
@@ -92,6 +48,8 @@ class BinaryTree {
      * проверка на существование элемента в дереве
      *
      * @value - значение элемента
+     *
+     * @return - возвращает true, если элемент существует
      */
     fun contains(value: Int) = containsRec(root, value)
 
@@ -99,54 +57,57 @@ class BinaryTree {
      * обход в глубину
      *
      * сначала левый дочерний элемент, потом родитель, затем правый дочерний
+     *
+     * @return возвращает элементы дерева
      */
-    fun traverseInOrder() = traverseInOrderRec(root)
+    fun traverseInOrder() : List<Int> {
+        return mutableListOf<Int>().apply {
+            traverseInOrderRec(root, this)
+        }
+    }
 
     /**
      * обход в глубину
      *
      * сначала родитель, затем левый и правый дочерние элементы
+     *
+     * @return возвращает элементы дерева
      */
-    fun traversePreOrder() = traversePreOrderRec(root)
+    fun traversePreOrder() : List<Int> {
+        return mutableListOf<Int>().apply {
+            traversePreOrderRec(root, this)
+        }
+    }
 
     /**
      * обход в глубину
      *
      * сначала левый и правый дочерние элементы, затем родитель
+     *
+     * @return возвращает элементы дерева
      */
-    fun traversePostOrder() = traversePostOrderRec(root)
-
-    /**
-     * обход в ширину
-     */
-    fun traverseLevelOrder() {
-        val root = this.root ?: return
-
-        val queue = LinkedList<Node>()
-        queue.add(root)
-
-        while (queue.isNotEmpty()) {
-            val node = queue.remove()
-            println("${node.value()} ")
-            if (node.leftNode() != null) {
-                queue.add(node.leftNode()!!)
-            }
-            if (node.rightNode() != null) {
-                queue.add(node.rightNode()!!)
-            }
+    fun traversePostOrder() : List<Int> {
+        return mutableListOf<Int>().apply {
+            traversePostOrderRec(root, this)
         }
     }
 
     /**
-     * с помощью алгоритма обхода в ширину получает все элементы дерева и возвращает их
+     * обход в ширину
+     *
+     * использует дополнительную структуру данных - очередь, в которую добавляет новые узлы дерева
+     * до тех пор пока не добавит последний узел
+     *
+     * @return возвращает элементы дерева
      */
-    fun items() : List<Int> {
+    fun traverseLevelOrder() : List<Int> {
         val root = this.root ?: return listOf()
 
         val queue = LinkedList<Node>()
         queue.add(root)
 
         val items = mutableListOf<Int>()
+
         while (queue.isNotEmpty()) {
             val node = queue.remove()
             items.add(node.value())
@@ -157,30 +118,36 @@ class BinaryTree {
                 queue.add(node.rightNode()!!)
             }
         }
+
         return items
     }
 
-    private fun traverseInOrderRec(node: Node?) {
+    /**
+     * большая часть алгоритмов с деревьями реализована в виде дополнительных рекурсивных функций
+     *
+     */
+
+    private fun traverseInOrderRec(node: Node?, nodes: MutableList<Int>) {
         if (node != null) {
-            traverseInOrderRec(node.leftNode())
-            println("${node.value()} ")
-            traverseInOrderRec(node.rightNode())
+            traverseInOrderRec(node.leftNode(), nodes)
+            nodes.add(node.value())
+            traverseInOrderRec(node.rightNode(), nodes)
         }
     }
 
-    private fun traversePreOrderRec(node: Node?) {
+    private fun traversePreOrderRec(node: Node?, nodes: MutableList<Int>) {
         if (node != null) {
-            println("${node.value()} ")
-            traversePreOrderRec(node.leftNode())
-            traversePreOrderRec(node.rightNode())
+            nodes.add(node.value())
+            traversePreOrderRec(node.leftNode(), nodes)
+            traversePreOrderRec(node.rightNode(), nodes)
         }
     }
 
-    private fun traversePostOrderRec(node: Node?) {
+    private fun traversePostOrderRec(node: Node?, nodes: MutableList<Int>) {
         if (node != null) {
-            traversePostOrderRec(node.leftNode())
-            traversePostOrderRec(node.rightNode())
-            println("${node.value()} ")
+            traversePostOrderRec(node.leftNode(), nodes)
+            traversePostOrderRec(node.rightNode(), nodes)
+            nodes.add(node.value())
         }
     }
 
@@ -244,4 +211,53 @@ class BinaryTree {
         return current
     }
 
+}
+
+/**
+ * представляет узел дерева
+ *
+ * @value - значение узла
+ * @left - левый дочерний узел
+ * @right - правый дочерний узел
+ */
+class Node(
+    private var value: Int,
+    private var left: Node? = null,
+    private var right: Node? = null
+) {
+    /**
+     * возвращает значение узла
+     */
+    fun value() = value
+
+    /**
+     * изменяет значение узла
+     */
+    fun changeValue(value: Int) {
+        this.value = value
+    }
+
+    /**
+     * изменяет левый дочерний узел
+     */
+    fun changeLeft(left: Node?) {
+        this.left = left
+    }
+
+    /**
+     * изменяет правый дочерний узел
+     */
+    fun changeRight(right: Node?) {
+        this.right = right
+    }
+
+    /**
+     * возвращает левый дочерний узел
+     */
+    fun leftNode() = left
+
+    /**
+     * возвращает правый дочерний узел
+     */
+    fun rightNode() = right
 }
--- src/main/kotlin/structures/DynamicArray.kt
+++ src/main/kotlin/structures/DynamicArray.kt
@@ -3,14 +3,23 @@ package structures
 import java.lang.IllegalStateException
 
 /**
- * структура данных динамический массив
+ * структура данных: динамический массив
  *
+ * описание: обертка над обычным массивом, в которой осуществляется проверка индексов
+ * и при переполнении массива, его размер увеличивается динамически
  */
 
 class DynamicArray(private var capacity: Int = 10) {
     private var data = Array(capacity) { 0 }
     private var index = 0
 
+    /**
+     * добавляет новый элемент в массив
+     *
+     * если массив не может вместить новый элемент, то его размер динамически увеличивается
+     *
+     * @value - элемент
+     */
     fun add(value: Int) {
         if (index < data.size - 1) {
             data[index++] = value
@@ -20,6 +29,11 @@ class DynamicArray(private var capacity: Int = 10) {
         }
     }
 
+    /**
+     * удаляет элемент из массива и сдвигает на его место последующие
+     *
+     * @value - элемент
+     */
     fun remove(value: Int) : Boolean {
         val foundedIndex = data.indexOf(value)
         if (foundedIndex == -1) {
@@ -31,8 +45,23 @@ class DynamicArray(private var capacity: Int = 10) {
         return true
     }
 
+    /**
+     * проверяет на существование элемента в массиве
+     *
+     * @value - элемент
+     *
+     * @return возвращает true, если элемент присутствует в массиве
+     */
     fun contains(value: Int) = data.contains(value)
 
+    /**
+     * устанавливает новое значения элемента для указанного индекса
+     *
+     * @index - индекс элемента
+     * @value - новое значение элемента
+     *
+     * @return возвращает true, если элемент был успешно изменен
+     */
     fun set(index: Int, value: Int) : Boolean {
         if (isBound(index)) {
             data[index] = value
@@ -41,6 +70,14 @@ class DynamicArray(private var capacity: Int = 10) {
         return false
     }
 
+
+    /**
+     * возвращает значение элемента по индексу или генерирует исключение, если некорректный индекс
+     *
+     * @index - индекс элемента
+     *
+     * @return возвращает значение элемента по индексу
+     */
     fun get(index: Int) : Int {
         if (isBound(index)) {
             return data[index]
@@ -49,12 +86,25 @@ class DynamicArray(private var capacity: Int = 10) {
         }
     }
 
+    /**
+     * возвращает размер массива
+     *
+     * @return возвращает размер массива
+     */
     fun capacity() = capacity
 
-    private fun isBound(index: Int) = index >= 0 && index < data.size
+    /**
+     * проверка на корретный индекс
+     *
+     * @return возвращает true, если индекс входит в диапазон доступных индексов
+     */
+    private fun isBound(i: Int) = i in 0 until index
 
     override fun toString() = data.joinToString(", ")
 
+    /**
+     * увеличивает размер массива, когда его нехватает
+     */
     private fun increaseSize() {
         capacity *= 2
         val newArray = Array(capacity) { 0 }
