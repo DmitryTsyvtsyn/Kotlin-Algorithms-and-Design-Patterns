72512f99a573e37112a11a680579be6fe93b9c8f
DmitryTsyvtsyn <dmitry.kind.2@gmail.com>
2021-11-08 09:40:12 +0600

    добавление строк доки для Euclid, FizzBuzz, ReverseArray и SieveOfEratosthenes

--- src/main/kotlin/other/ReverseArray.kt
+++ src/main/kotlin/other/ReverseArray.kt
@@ -1,7 +1,19 @@
 package other
 
+/**
+ * алгоритм переворота массива
+ *
+ * сложность алгоритма: n/2 операций
+ */
+
 class ReverseArray<T> {
 
+    /**
+     * создает и возвращает обратный массив для входного
+     *
+     * @array - входной массив
+     * @return возвращает обратный массив
+     */
     fun compute(array: Array<T>) : Array<T> {
         val newArray = array.copyOf()
         for (index in 0 until array.size / 2) {
--- src/main/kotlin/other/SieveOfEratosthenes.kt
+++ src/main/kotlin/other/SieveOfEratosthenes.kt
@@ -1,14 +1,26 @@
 package other
 
+/**
+ * решето Эратосфена позволяет эффективно вычислить ряд простых чисел
+ *
+ * сложность алгоритма: nlog(logn)) операций
+ */
+
 class SieveOfEratosthenes {
 
+    /**
+     * вычисляет ряд простых чисел для максимального значения
+     *
+     * @maxNumber - максимальное значение
+     * @return - возвращает список простых чисел
+     */
     fun compute(maxNumber: Int) : List<Int> {
         val numbers = Array(maxNumber + 1) { index -> index >= 2 }
 
         var x = 2
         while (x * x <= maxNumber) {
             if (numbers[x]) {
-                for (y in x*x..maxNumber step x) {
+                for (y in x * x..maxNumber step x) {
                     numbers[y] = false
                 }
             }
