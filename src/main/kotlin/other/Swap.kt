f4ae145389f6d6a0d1ba7aed524f2cc472307383
DmitryTsyvtsyn <dmitry.kind.2@gmail.com>
2021-11-11 14:56:13 +0600

    добавление доки для класса Swap

--- src/main/kotlin/other/Swap.kt
+++ src/main/kotlin/other/Swap.kt
@@ -1,11 +1,33 @@
 package other
 
+/**
+ * алгоритма обмена двух переменных без третьей дополнительной
+ *
+ */
+
 class Swap {
-    
+
+    /**
+     * Kotlin расширение для проверки индекса списка на корректность
+     *
+     * @return возвращает true, если элемент доступен по индексу
+     */
     private fun  List<Int>.inIndex(index: Int) = index in 0 until size
-    
+
+    /**
+     * Kotlin расширение для проверки индекса массива на корректность
+     *
+     * @return возвращает true, если элемент доступен по индексу
+     */
     private fun Array<Int>.inIndex(index: Int) = index in 0 until size
-    
+
+    /**
+     * меняет местами два значения списка по указанным индексам
+     *
+     * @list - список
+     * @old - индекс первого элемента
+     * @new - индекс второго элемента
+     */
     fun swap(list: MutableList<Int>, old: Int, new: Int) {
         if (list.inIndex(old) && list.inIndex(new)) {
             list[old] = list[old] + list[new]
@@ -14,6 +36,13 @@ class Swap {
         }
     }
 
+    /**
+     * меняет местами два значения массива по указанным индексам
+     *
+     * @list - массив
+     * @old - индекс первого элемента
+     * @new - индекс второго элемента
+     */
     fun swap(array: Array<Int>, old: Int, new: Int) {
         if (array.inIndex(old) && array.inIndex(new)) {
             array[old] = array[old] + array[new]
