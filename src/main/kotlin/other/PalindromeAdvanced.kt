ed73e877b17245693429771764794aa8c0c3f5f8
DmitryTsyvtsyn <dmitry.kind.2@gmail.com>
2022-02-03 12:03:56 +0700

    added advanced algorithm for palindrome

---
+++ src/main/kotlin/other/PalindromeAdvanced.kt
@@ -0,0 +1,34 @@
+package other
+
+/**
+ * Algorithm for checking a string for a palindrome
+ *
+ */
+
+class PalindromeAdvanced(private val text: String) {
+
+    /**
+     * checks a string for a palindrome
+     *
+     * @return returns true if the string is a palindrome
+     */
+    fun isYes() : Boolean {
+        if (text.length <= 1) {
+            return true
+        }
+        for (i in 0 until text.length / 2) {
+            if (text[i] != text[text.length - 1 - i]) {
+                return false
+            }
+        }
+        return true
+    }
+
+    /**
+     * checks if a string is not a palindrome
+     *
+     * @return returns true if the string is not a palindrome
+     */
+    fun isNot() = !isYes()
+
+}
--- src/test/kotlin/other/PalindromeAdvancedTest.kt
+++ src/test/kotlin/other/PalindromeAdvancedTest.kt
@@ -1,39 +1,40 @@
 package other
 
 import org.junit.Test
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 
-internal class PalindromeTest {
+class PalindromeAdvancedTest {
 
     @Test
     fun test_empty_string() {
         val text1 = ""
-        assertEquals(Palindrome(text1).isYes(), true)
+        assertEquals(PalindromeAdvanced(text1).isYes(), true)
     }
 
     @Test
     fun test_one_length() {
         val text1 = "a"
-        assertEquals(Palindrome(text1).isYes(), true)
+        assertEquals(PalindromeAdvanced(text1).isYes(), true)
     }
 
     @Test
     fun test_is_palindrome() {
         val text1 = "tenet"
-        assertEquals(Palindrome(text1).isYes(), true)
+        assertEquals(PalindromeAdvanced(text1).isYes(), true)
 
         val text2 = "friend"
-        assertEquals(Palindrome(text2).isYes(), false)
+        assertEquals(PalindromeAdvanced(text2).isYes(), false)
     }
 
     @Test
     fun test_is_not_palindrome() {
         val text1 = "white"
-        assertEquals(Palindrome(text1).isNot(), true)
+        assertEquals(PalindromeAdvanced(text1).isNot(), true)
 
         val text2 = "tenet"
-        assertEquals(Palindrome(text2).isNot(), false)
+        assertEquals(PalindromeAdvanced(text2).isNot(), false)
     }
 
 }
+
+
