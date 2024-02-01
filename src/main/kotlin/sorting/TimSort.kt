5677141aea6c676d9ba1c240adb43d067b00bf9b
DmitryTsyvtsyn <dmitry.kind.2@gmail.com>
2024-02-01 17:26:56 +0700

    refactored TimSort algorithm

--- src/main/kotlin/sorting/TimSort.kt
+++ src/main/kotlin/sorting/TimSort.kt
@@ -1,110 +1,126 @@
 package sorting
 
-import java.lang.Integer.min
+import kotlin.math.min
 
 /**
- * Tim sort algorithm
  *
- * the best time: n
- * average time: n * log(n)
+ * Tim Sort is a hybrid sorting algorithm derived from merge sort and insertion sort
+ *
+ * used by Python’s sorted() and list.sort() functions, which was designed to perform well on
+ *
+ * many kinds of real-world data.
+ *
  * worst time: n * log(n)
+ * best time: n
+ * average time: n * log(n)
  *
  * amount of memory: n
  *
  */
 
-fun Array<Int>.timSort() {
-    val size = size
-    val minrun = minrun(size)
-    var index = 0
-    while (index < size) {
-        insertionSort(this, index, min(index + minrun - 1, size - 1))
-        index += minrun
-    }
+class TimSort {
 
-    var mergingSize = minrun
-    while (mergingSize < size) {
+    fun sort(array: Array<Int>) {
+        val arraySize = array.size
+        val minRunSize = minRunSize(arraySize)
 
-        var left = 0
-        while (left < size) {
-            val middle = left + mergingSize - 1
-            val right = min(left + 2 * mergingSize - 1, size - 1)
-            if (middle < right) {
-                merge(this, left, middle, right)
+        var i = 0
+        while (i < arraySize) {
+            insertionSort(array, i, min(i + minRunSize - 1, arraySize - 1))
+            i += minRunSize
+        }
+
+        var mergingSize = minRunSize
+        while (mergingSize < arraySize) {
+            var start = 0
+            while (start < arraySize) {
+                val middle = start + mergingSize - 1
+                val end = min(start + 2 * mergingSize - 1, arraySize - 1)
+                if (middle < end) {
+                    merge(array, start, middle, end)
+                }
+                start += mergingSize * 2
             }
-            left += mergingSize * 2
+            mergingSize *= 2
+        }
+    }
+
+    /**
+     * Minrun is chosen from the range 32 to 64 inclusive, such that the size of the data, divided by minrun, is equal to,
+     * or slightly less than, a power of two. The final algorithm takes the six most significant bits of the size of the array,
+     * adds one if any of the remaining bits are set, and uses that result as the minrun.
+     * This algorithm works for all arrays, including those smaller than 64; for arrays of size 63 or less,
+     * this sets minrun equal to the array size and Timsort reduces to an insertion sort
+     */
+    private fun minRunSize(arraySize: Int) : Int {
+        var result = 0
+        var size = arraySize
+        while (size >= 64) {
+            result = result or (size and 1)
+            // shift one bit to the right until 6 significant bits remain
+            size = size shr 1
         }
 
-        mergingSize *= 2
+        return size + result
     }
-}
 
-private fun minrun(n: Int) : Int {
-    var addedValue = 0
-    var size = n
-    if (size >= 64) {
-        addedValue = addedValue or (size and 1)
-        size = size.shr(1)
-    }
-    return size + addedValue
-}
-
-private fun insertionSort(array: Array<Int>, left: Int, right: Int) {
-    var outerIndex = left + 1
-    while (outerIndex <= right) {
-        val temporaryValue = array[outerIndex]
-        var innerIndex = outerIndex - 1
-        while (innerIndex >= left && array[innerIndex] > temporaryValue) {
-            array[innerIndex + 1] = array[innerIndex]
-            innerIndex--
+    private fun insertionSort(array: Array<Int>, start: Int, end: Int) {
+        var i = start + 1
+        while (i <= end) {
+            val current = array[i]
+            var j = i - 1
+            while (j >= start && array[j] > current) {
+                array[j + 1] = array[j]
+                j--
+            }
+            array[j + 1] = current
+            i++
         }
-        array[innerIndex + 1] = temporaryValue
-        outerIndex++
-    }
-}
-
-private fun merge(array: Array<Int>, left: Int, middle: Int, right: Int) {
-    val leftLengthArray = middle - left + 1
-    val rightLengthArray = right - middle
-
-    var index = 0
-    var leftArray = Array(leftLengthArray) { 0 }
-    while (index < leftLengthArray) {
-        leftArray[index] = array[left + index]
-        index++
     }
 
-    index = 0
-    var rightArray = Array(rightLengthArray) { 0 }
-    while (index < rightLengthArray) {
-        rightArray[index] = array[middle + 1 + index]
-        index++
-    }
+    private fun merge(array: Array<Int>, start: Int, middle: Int, end: Int) {
+        val leftSize = middle - start + 1
+        val rightSize = end - middle
 
-    var leftArrayIndex = 0
-    var rightArrayIndex = 0
-    index = 0
-
-    while (leftArrayIndex < leftLengthArray && rightArrayIndex < rightLengthArray) {
-        if (leftArray[leftArrayIndex] <= rightArray[rightArrayIndex]) {
-            array[index] = leftArray[leftArrayIndex]
-            leftArrayIndex++
-        } else {
-            array[index] = rightArray[rightArrayIndex]
-            rightArrayIndex++
+        var i = 0
+        val leftArray = Array(leftSize) { 0 }
+        while (i < leftSize) {
+            leftArray[i] = array[start + i]
+            i++
         }
-        index++
+
+        i = 0
+        val rightArray = Array(rightSize) { 0 }
+        while (i < rightSize) {
+            rightArray[i] = array[middle + i + 1]
+            i++
+        }
+
+        i = 0
+        var j = 0
+        var k = start
+        while (i < leftSize && j < rightSize) {
+            if (leftArray[i] <= rightArray[j]) {
+                array[k] = leftArray[i]
+                i++
+            } else {
+                array[k] = rightArray[j]
+                j++
+            }
+            k++
+        }
+
+        while (i < leftSize) {
+            array[k] = leftArray[i]
+            i++
+            k++
+        }
+
+        while (j < rightSize) {
+            array[k] = rightArray[j]
+            j++
+            k++
+        }
     }
 
-    while (leftArrayIndex < leftLengthArray) {
-        array[index] = leftArray[leftArrayIndex]
-        leftArrayIndex++
-        index++
-    }
-
-    while (rightArrayIndex < rightLengthArray) {
-        array[index] = rightArray[rightArrayIndex]
-        rightArrayIndex++
-        index++
-    }
 }
--- src/test/kotlin/sorting/TimSortTest.kt
+++ src/test/kotlin/sorting/TimSortTest.kt
@@ -1,51 +1,35 @@
 package sorting
 
 import org.junit.Test
-import org.junit.Assert.assertEquals
+import kotlin.random.Random
+import org.junit.Assert.assertArrayEquals
 
 class TimSortTest {
 
     @Test
-    fun test_reversed_array() {
-        val expected = TestUtils.list(100000)
+    fun `test sort`() {
+        val timSort = TimSort()
 
-        val actual = expected.reversed().toTypedArray()
-        actual.timSort()
+        val expected1 = Array(1_000_000) { it }
+        val actual1 = expected1.reversedArray()
+        timSort.sort(actual1)
+        assertArrayEquals(expected1, actual1)
 
-        assertEquals(expected, actual.toList())
-    }
+        val actual2 = Array(1_000_000) { Random.nextInt(1_000_000) }
+        val expected2 = actual2.sortedArray()
+        timSort.sort(actual2)
+        assertArrayEquals(expected2, actual2)
 
-    @Test
-    fun test_random_array() {
-        val actual = TestUtils.randomArray(50000)
+        val expected3 = Array(1_000_000) { it }
+        val actual3 = expected3.copyOf()
+        actual3.shuffle()
+        timSort.sort(actual3)
+        assertArrayEquals(expected3, actual3)
 
-        val expected = actual.sorted()
-
-        actual.timSort()
-
-        assertEquals(expected, actual.toList())
-    }
-
-    @Test
-    fun test_shuffled_array() {
-        val expected = TestUtils.sortedArray(100000)
-
-        val actual = expected.copyOf()
-        actual.shuffle()
-        actual.timSort()
-
-        assertEquals(expected.toList(), actual.toList())
-    }
-
-    @Test
-    fun test_sorted_array() {
-        val actual = TestUtils.sortedArray(100000)
-
-        val expected = actual.toList()
-
-        actual.timSort()
-
-        assertEquals(expected, actual.toList())
+        val expected4 = Array(1_000_000) { it }
+        val actual4 = expected3.copyOf()
+        timSort.sort(actual3)
+        assertArrayEquals(expected4, actual4)
     }
 
 }
